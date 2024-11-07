914c80c8230d9f032ddaf4ee6f5146623249a540
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-11-07 14:50:34 -0600

    Update

---
+++ src/main/java/edu/miu/custommysterybox/CustomMysteryBoxApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.custommysterybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomMysteryBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomMysteryBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/custommysterybox/CustomMysteryBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.custommysterybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomMysteryBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
